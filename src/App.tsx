8c6721dfa66b31c4778827590990c8ce86cc3aad
linason-esc <linason7889@gmail.com>
2024-07-06 12:30:25 +0900

    chore: 초기 세팅 수정(패키지, prettier, 전체 css, 라우팅)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import DiaryHome from './DiaryHome';
 import DiaryOpen from './DiaryOpen';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<DiaryHome />} />
-          <Route path="/open" element={<DiaryOpen />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<DiaryHome />} />
+        <Route path="/open" element={<DiaryOpen />} />
+      </Routes>
+    </div>
   );
 }
 
